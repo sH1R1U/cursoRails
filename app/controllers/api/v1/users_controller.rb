6fbe91ccce9d6520a654a58fff759843842dd9da
Alexis <gomez4d@gmail.com>
2017-02-13 17:16:37 -0300

    controller de usuario modificado

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,18 @@
 #  Controlador para los users
 class Api::V1::UsersController < ApplicationController
 
+  #  Get /users/1
+  def show
+  	render json: @users
+  end
+
   #  PATCH/PUT /users/1.json
   def update
   	if @users.update(user_params)
-  		respond_with({'status':'succes'})
+  		render json: @users
   	else
-  		respond_with({'status':error})
+  		render json: @users.errors, status: :unprocessable_entity
+    end
   end
+
 end
