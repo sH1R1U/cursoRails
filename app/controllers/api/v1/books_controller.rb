cf56fac853eb67d75fef113e6ac835cabce275df
Alexis <gomez4d@gmail.com>
2017-02-13 20:37:34 -0300

    se crean los crud para los libros

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,4 +7,30 @@ class Api::V1::BooksController < ApplicationController
     render json: @books
   end
 
+  def show
+    render json: @book
+  end
+
+  def create
+    @book = Book.new(book_params) 
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity      
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book, status: :unprocessable_entity      
+    end
+  end
+
+  def destroy
+   @book.destroy 
+  end
+
 end
