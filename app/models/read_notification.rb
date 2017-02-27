4ac9e1e2e49075587aa62b8418535198b68a47ee
Alexis <gomez4d@gmail.com>
2017-02-27 20:02:36 -0300

    se corrigen los errores de relaciones

--- app/models/read_notification.rb
+++ app/models/read_notification.rb
@@ -1,5 +1,5 @@
 #  Modelo para verificar si leyo o no la notificacion del usuario
 class ReadNotification < ApplicationRecord
-  belongs_to :users
-  has_many :notifications
+  belongs_to :user
+  belongs_to :notification
 end
